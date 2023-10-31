b36cdcf7fc703aa4ea9f733f74ea4b49be68286d
Arthur Santos <arthurguilherme348@gmail.com>
2023-10-31 14:15:29 -0300

    SmartTv commit 1

---
+++ SmarTv/SmartTv/src/User.java
@@ -0,0 +1,10 @@
+public class User {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv(); 
+
+        System.out.println("Estado: " + smartTv.ligada);
+        System.out.println("Canal: " + smartTv.canal);
+        System.out.println("Volume: : " + smartTv.volume);
+
+    }
+}
--- TrilhaDio/src/Vars.java
+++ TrilhaDio/src/Vars.java
@@ -1,13 +1,15 @@
 public class Vars {
+    /* Entendendo o funcionamento de Variaveis  */
     byte idade = 123; /* 1 byte */
     short ano = 2021; /* 2 bytes  */
     int cep = 54100310;/* 4 bytes */
     long cpf = 98765432109L; /* 8 bytes */
     float PI = 3.14F; /* 4 bytes */
     double salario = 1275.33;  /* 8 bytes */
+
     char letra = 'a'; 
     String nome= "Arthur Santos";
-
+    
     final String PAIS = "BR"; /* A palavra fina, torna pais como constante*/
 
 }
