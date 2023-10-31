a93711ba3f6a1a3b54aac7633b01b92ca71f7d0f
Arthur Santos <arthurguilherme348@gmail.com>
2023-10-31 11:53:01 -0300

    Vars

---
+++ TrilhaDio/src/Vars.java
@@ -0,0 +1,10 @@
+public class Vars {
+    public static void main() {
+        byte idade = 123; /* 1 byte */
+        short ano = 2021; /* 2 bytes  */
+        int cep = 54100310;/* 4 bytes */
+        long cpf = 98765432109L; /* 8 bytes */
+        float PI = 3.14F; /* 4 bytes */
+        double salario = 1275.33;  /* 8 bytes */
+    }
+}
