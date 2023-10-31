00450dbd9e948f2f65397775e32854dd24a5948d
Arthur Santos <arthurguilherme348@gmail.com>
2023-10-31 16:35:55 -0300

    Scan

--- TrilhaDio/src/AboutMe.java
+++ TrilhaDio/src/AboutMe.java
@@ -1,15 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
-        String nome = args[0]; 
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]); 
+            Scanner scan = new Scanner(System.in).useLocale(Locale.US); 
+            System.out.println("Digite o seu nome: ");
+            String nome = scan.next();
 
-        System.out.println(nome);
-        System.out.println(sobrenome);
-        System.out.println(nome);
-        System.out.println(nome);
-        
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = scan.next();
+
+            System.out.println("Digite sua idade: ");
+            int idade = scan.nextInt();
+
+            System.out.println("Digite sua altura: ");
+            Double altura = scan.nextDouble();
+
+            System.err.println("Seu nome é " + nome + " " + sobrenome + ", você tem " + idade + "anos" + " e mede " + altura +"m.");
+        }
     }
     
-}
